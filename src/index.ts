f31472c4cdea9c30c6e74933c540c5fe97601ce2
Finn Poppinga <finno.ger@gmail.com>
2018-05-05 14:42:31 +0200

    Support ynab API

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,18 @@ import * as fs from 'mz/fs';
 import {YNABConverter} from './io/ynabConverter';
 import * as path from 'path';
 import {Command} from 'commander';
+import {config as initEnvironment} from "dotenv";
+import {YnabClient} from './io/ynabClient';
+
+initEnvironment();
 
 program
-    .version('1.0.0')
-    .command('<input> [<output>]', 'parse StarMoney export format and output it as YNAB format.')
-    .action(async (input : string, output : string | Command) => {
+    .version('2.0.0');
+
+program
+    .command('convert <input> [<output>]')
+    .description('parse StarMoney export format and output it as YNAB format.')
+    .action((async (input: string, output: string | Command) => {
         if (typeof output !== "string") {
             output = input.replace(/\.txt$/, ".csv");
         }
@@ -24,6 +31,47 @@ program
             await fs.writeFile(path.resolve(output), result);
         } catch (err) {
             console.error("Error when converting file:", err);
+            process.exit(1);
         }
-    })
-    .parse(process.argv);
+    }));
+
+program
+    .command("push <input>")
+    .option("-t, --token <token>", `
+        Your YNAB access token. Don't have one? 
+        Go to https://app.youneedabudget.com/settings/developer to get one.
+        
+        You can alternatively just set the environment variable YNAB_TOKEN to the value of your token.`)
+    .option("-b, --budget <budget>", `
+        The name of your YNAB Budget. If you don't provide a budget, the one with the most 
+        recent change will be used.`)
+    .option("-a, --account <account>", `
+        Your account's name. Partial name is enough.
+    `)
+    .description("parse StarMoney export format and push contents to YNAB directly.")
+    .action(async (input: string, command: Command) => {
+        const token = command.opts()["token"] || process.env["YNAB_TOKEN"];
+        if (!token) {
+            console.error("You have to provide a YNAB token.");
+            return process.exit(1);
+        }
+
+        const accountName = command.opts()["account"];
+        if (!accountName) {
+            console.error("You have to provide an account name.");
+            return process.exit(1);
+        }
+
+        try {
+            const file = await fs.readFile(path.resolve(input));
+            const reader = new CsvReader(StarmoneySeparators, StarmoneyFields, new StarmoneyMapping(), file.toString('utf-8'));
+
+            const ynabClient = new YnabClient(token, accountName, command.opts()["budget"]);
+            await ynabClient.createTransactions([...reader.read()]);
+        } catch (err) {
+            console.error("Error when pushing: ", err);
+            process.exit(1);
+        }
+    });
+
+program.parse(process.argv);
---
+++ src/io/ynabClient.ts
@@ -0,0 +1,75 @@
+import {api as YNABApi, SaveTransaction} from "ynab"
+import {YNABRecord} from '../model/ynab';
+
+export class YnabClient {
+    private api: YNABApi;
+    private _accountId: string | null = null;
+
+    constructor(private appToken: string, private accountName: string, private budgetId: string | null = null) {
+        this.api = new YNABApi(appToken);
+    }
+
+    async createTransactions(transactions: Array<YNABRecord>): Promise<void> {
+        try {
+            const budgetId = await this.getBudgetId();
+
+            const response = await this.api.transactions.bulkCreateTransactions(budgetId, {
+                transactions: await Promise.all(transactions.map(t => this.convertTransaction(t)))
+            });
+
+            console.info("response: ", response);
+        } catch (e) {
+            console.error("Error when creating transactions: ", e);
+        }
+    }
+
+    private async getBudgetId(): Promise<string> {
+        if (this.budgetId) {
+            return this.budgetId;
+        }
+
+        const response = await this.api.budgets.getBudgets();
+
+        return response.data.budgets.sort((a, b) => {
+            if (!a.last_modified_on || !b.last_modified_on) {
+                return 0;
+            }
+
+            return a.last_modified_on.localeCompare(b.last_modified_on);
+        })[0].id;
+    }
+
+    private async getAccountId(name: string): Promise<string> {
+        if (this._accountId) {
+            return this._accountId;
+        }
+
+        const accounts = await this.api.accounts.getAccounts(await this.getBudgetId());
+
+        const matchingAccounts = accounts.data.accounts.filter(a => RegExp(name, "i").test(a.name));
+        if (matchingAccounts.length > 1) {
+            console.error("Account name is ambiguos. Candidates: ", matchingAccounts.map(a => a.name));
+            throw new Error("AMBIGUOUS_ACCOUNT");
+        }
+
+        const matchingAccount = matchingAccounts[0];
+
+        if (!matchingAccount) {
+            console.error("Can't find a matching account! Possibilities:", accounts.data.accounts.map(a => a.name));
+            throw new Error("NO_MATCHING_ACCOUNT");
+        }
+
+        this._accountId = matchingAccount.id;
+        return matchingAccount.id;
+    }
+
+    private async convertTransaction(t: YNABRecord): Promise<SaveTransaction> {
+        return {
+            account_id: await this.getAccountId(this.accountName),
+            date: t.date.toISOString(),
+            amount: (t.inflow - t.outflow) * 1e3,
+            memo: t.memo.slice(0, 100),
+            cleared: SaveTransaction.ClearedEnum.Cleared
+        }
+    }
+}
